5e514c262d35c520c85e3749c52ab906276ec936
oscar Velez <ovelez_24@hotmail.com>
2022-11-10 21:59:50 -0500

    cambio2

--- sintragnb/resources/views/index.blade.php
+++ sintragnb/resources/views/index.blade.php
@@ -8,6 +8,8 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/educaciongrande.jpg') }}">
+    <link rel="stylesheet" href="{{ asset('assets/viviendagrande.jpg') }}">
     <title>SintraGNB</title>
   </head>
   <body>
@@ -19,26 +21,75 @@
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-        <div class="dropdown">
-            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="true">
-                Dropdown link
+    <ul class="navbar-nav ml-auto">
+
+        <div class="dropdown" >
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="true" >
+
             </a>
 
-            <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
+            <div class="dropdown-menu" >
+                <a class="dropdown-item" href="#">Texto Convencional</a>
+                <a class="dropdown-item" href="#">Póliza exequial</a>
+                <a class="dropdown-item" href="#">Cuadro de beneficios economicos</a>
+                <a class="dropdown-item" href="#">Seguros</a>
             </div>
         </div>
+
+        <li class="nav-item" class="pb-4 pt-4">
+          <a class="nav-link" href="#">Educacion</a>
+        </li>
+        <li class="nav-item" class="pb-4 pt-4">
+          <a class="nav-link" href="#">Vivienda</a>
+        </li>
+        <li class="nav-item" class="pb-4 pt-4">
+          <a class="nav-link" href="#">Auxilios</a>
+        </li>
+        <li class="nav-item" class="pb-4 pt-4">
+          <a class="nav-link" href="#">Primas</a>
+        </li>
+        <li class="nav-item" class="pb-4 pt-4">
+          <a class="nav-link" href="#">Permisos</a>
+        </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
     </form>
   </div>
 </nav>
     <!--/Nabvar -->
+
+    <!-- main -->
+<main id= "main">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-pause="false">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <span>Educacion</span>
+                <img id="menu"  src="assets/educaciongrande.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+
+
+                </div>
+                <div class="carousel-item">
+                    <span>vivienda</span>
+                    <img src="assets/viviendagrande.jpg" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </button>
+        </div>
+
+</main>
+
+     <!--/ main -->
 
     <!--footer-->
        <footer class="pb-4 pt-4">
